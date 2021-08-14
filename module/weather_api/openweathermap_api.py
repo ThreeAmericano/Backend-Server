95c8a3e1a83228ff79b152a28c6360441af6fe32
FPast <chgy2131@naver.com>
2021-08-15 00:07:31 +0900

    develop

--- module/weather_api/openweathermap_api.py
+++ module/weather_api/openweathermap_api.py
@@ -4,18 +4,9 @@ import urllib.request
 import json
 import datetime
 
-openweather_api_url = "https://api.openweathermap.org/data/2.5/"
-service_key = "c7446d3b017961049805343e08347b43"
 
-dict_data = {'갱신일': str(datetime.datetime.now())}
-
-
-def getterDict():
-	global dict_data
-	return dict_data
-
-
-def __airpollution_policy(value, good, normal, bad):
+# 인자로 주어진 값(value)를 기준에 따라 좋음/보통/나쁨/매우나쁨 으로 반환하는 함수
+def airpollution_policy(value, good, normal, bad):
 	if value <= good:
 		return '좋음'
 	elif value <= normal:
@@ -26,94 +17,116 @@ def __airpollution_policy(value, good, normal, bad):
 		return '매우나쁨'
 
 
-def getNowCity(city) :
-	global openweather_api_url, service_key, dict_data
-	dict_data['갱신일'] = str(datetime.datetime.now())
+class OpenWeatherAPI:
+	openweather_api_url = "https://api.openweathermap.org/data/2.5/"
+	__service_key = "none"
+	__dict_data = {'갱신일': str(datetime.datetime.now())}
+	recent_lon = "none"
+	recent_lat = "none"
 
-	# API 요청시 필요한 인수값 정의
-	ow_api_url = openweather_api_url + "weather"
-	payload = "?q=" + city + "&" + "lang=kr&" + "appid=" + service_key
-	url_total = ow_api_url + payload
+	def __init__(self, service_key):
+		self.__service_key = service_key
 
-	# API 요청하여 데이터 받기
-	req = urllib.request.urlopen(url_total)
-	res = req.readline()
+	def getter_dict(self):
+		return self.__dict_data
 
-	# 받은 값 JSON 형태로 정제후 반환
-	items = json.loads(res)
-	dict_data['도시명'] = items['name']
-	dict_data['날씨'] = items['weather'][0]['main']
-	dict_data['날씨설명'] = items['weather'][0]['description']
-	dict_data['아이콘'] = items['weather'][0]['icon']
+	def getter_pos_lat(self):
+		return self.recent_lat
 
-	dict_data['현재온도'] = str(int(items['main']['temp'])-273.15)[0:4]
-	dict_data['체감온도'] = str(int(items['main']['feels_like'])-273.15)[0:4]
-	dict_data['최저온도'] = str(int(items['main']['temp_min'])-273.15)[0:4]
-	dict_data['최고온도'] = str(int(items['main']['temp_max'])-273.15)[0:4]
+	def getter_pos_lon(self):
+		return self.recent_lon
 
-	dict_data['습도'] = str(items['main']['humidity']) + "%"
-	dict_data['기압'] = str(items['main']['pressure']) + "hPa"
-	dict_data['가시거리'] = items['visibility']
+	def update_now_auto(self, city):
+		self.update_weather_city(city)
+		self.update_air_pollution(self.recent_lat, self.recent_lon)
 
-	dict_data['풍속'] = str(items['wind']['speed']) + "m/s"
-	dict_data['풍향'] = str(items['wind']['deg']) + "도"
-	#dict_data['돌풍'] = items['wind']['gust']
-	#dict_data['1시간강수'] = items['rain']['1h']) #비 올때만 생김
-	#dict_data['3시간강수'] = items['rain']['3h']) #비 올때만 생김
-	#dict_data['1시간적설'] = items['snow']['1h']) #눈 올때만 생김
-	#dict_data['3시간적설'] = items['snow']['3h']) #눈 올때만 생김
-	dict_data['흐림'] = str(items['clouds']['all']) + "%"
-	dict_data['일출'] = items['sys']['sunrise']
-	dict_data['일몰'] = items['sys']['sunset']
+	def update_weather_city(self, city):
+		self.__dict_data['갱신일'] = str(datetime.datetime.now())
 
-	# 미세먼지 값 불러오기 (대기상태)
-	getNowAirPollution(str(items['coord']['lat']), str(items['coord']['lon']))
+		# API 요청시 필요한 인수값 정의
+		ow_api_url = self.openweather_api_url + "weather"
+		payload = "?q=" + city + "&" + "lang=kr&" + "appid=" + self.__service_key
+		url_total = ow_api_url + payload
 
+		# API 요청하여 데이터 받기
+		req = urllib.request.urlopen(url_total)
+		res = req.readline()
 
-def getNowAirPollution(pos_lat, pos_lon):
-	global openweather_api_url, service_key, dict_data
-	dict_data['갱신일'] = str(datetime.datetime.now())
+		# 받은 값 JSON 형태로 정제후 반환
+		items = json.loads(res)
+		self.recent_lat = str(items['coord']['lat'])
+		self.recent_lon = str(items['coord']['lon'])
 
-	# API 요청시 필요한 인수값 정의
-	ow_api_url = openweather_api_url + "air_pollution"
-	payload = "?lat=" + pos_lat + "&" +\
-				"lon=" + pos_lon + "&" +\
-				"appid=" + service_key
-	url_total = ow_api_url + payload
+		self.__dict_data['도시명'] = items['name']
+		self.__dict_data['날씨'] = items['weather'][0]['main']
+		self.__dict_data['날씨설명'] = items['weather'][0]['description']
+		self.__dict_data['아이콘'] = items['weather'][0]['icon']
 
-	# API 요청하여 데이터 받기
-	req = urllib.request.urlopen(url_total)
-	res = req.readline()
+		self.__dict_data['현재온도'] = str(int(items['main']['temp'])-273.15)[0:4]
+		self.__dict_data['체감온도'] = str(int(items['main']['feels_like'])-273.15)[0:4]
+		self.__dict_data['최저온도'] = str(int(items['main']['temp_min'])-273.15)[0:4]
+		self.__dict_data['최고온도'] = str(int(items['main']['temp_max'])-273.15)[0:4]
 
-	# 받은 값 JSON 형태로 정제후 반환
-	items = json.loads(res)
+		self.__dict_data['습도'] = str(items['main']['humidity']) + "%"
+		self.__dict_data['기압'] = str(items['main']['pressure']) + "hPa"
+		self.__dict_data['가시거리'] = items['visibility']
 
-	# 대기질 기준 : https://www.airkorea.or.kr/web/khaiInfo?pMENU_NO=129
-	#air_co = airpollution_policy(items['list'][0]['components']['co'], 16, 36, 76)
-	#air_no2 = airpollution_policy(items['list'][0]['components']['no2'], 0.031, 0.061, 0.201)
-	air_total = __airpollution_policy(items['list'][0]['main']['aqi'], 1, 2, 3)
-	air_pm25 = __airpollution_policy(items['list'][0]['components']['pm2_5'], 15, 35, 75)
-	air_pm10 = __airpollution_policy(items['list'][0]['components']['pm10'], 30, 80, 150)
-	#air_o3 = airpollution_policy(items['list'][0]['components']['o3'], 0.031, 0.061, 0.201)
-	#air_so2 = airpollution_policy(items['list'][0]['components']['so2'], 0.031, 0.061, 0.201)
+		self.__dict_data['풍속'] = str(items['wind']['speed']) + "m/s"
+		self.__dict_data['풍향'] = str(items['wind']['deg']) + "도"
+		# self.__dict_data['돌풍'] = items['wind']['gust']
+		# self.__dict_data['1시간강수'] = items['rain']['1h']) #비 올때만 생김
+		# self.__dict_data['3시간강수'] = items['rain']['3h']) #비 올때만 생김
+		# self.__dict_data['1시간적설'] = items['snow']['1h']) #눈 올때만 생김
+		# self.__dict_data['3시간적설'] = items['snow']['3h']) #눈 올때만 생김
+		self.__dict_data['흐림'] = str(items['clouds']['all']) + "%"
+		self.__dict_data['일출'] = items['sys']['sunrise']
+		self.__dict_data['일몰'] = items['sys']['sunset']
 
-	dict_data['대기질지수'] = air_total
-	dict_data['미세먼지'] = air_pm10
-	dict_data['초미세먼지'] = air_pm25
-	dict_data['co'] = items['list'][0]['components']['co']
-	dict_data['no'] = items['list'][0]['components']['no']
-	dict_data['no2'] = items['list'][0]['components']['no2']
-	dict_data['o3'] = items['list'][0]['components']['o3']
-	dict_data['so2'] = items['list'][0]['components']['so2']
-	dict_data['pm2_5'] = items['list'][0]['components']['pm2_5']
-	dict_data['pm10'] = items['list'][0]['components']['pm10']
-	dict_data['nh3'] = items['list'][0]['components']['nh3']
+		# 미세먼지 값 불러오기 (대기상태)
+
+	def update_air_pollution(self, pos_lat, pos_lon):
+		self.__dict_data['갱신일'] = str(datetime.datetime.now())
+
+		# API 요청시 필요한 인수값 정의
+		ow_api_url = self.openweather_api_url + "air_pollution"
+		payload = "?lat=" + pos_lat + "&" +\
+					"lon=" + pos_lon + "&" +\
+					"appid=" + self.__service_key
+		url_total = ow_api_url + payload
+
+		# API 요청하여 데이터 받기
+		req = urllib.request.urlopen(url_total)
+		res = req.readline()
+
+		# 받은 값 JSON 형태로 정제후 반환
+		items = json.loads(res)
+
+		# 대기질 기준 : https://www.airkorea.or.kr/web/khaiInfo?pMENU_NO=129
+		# air_co = airpollution_policy(items['list'][0]['components']['co'], 16, 36, 76)
+		# air_no2 = airpollution_policy(items['list'][0]['components']['no2'], 0.031, 0.061, 0.201)
+		air_total = airpollution_policy(items['list'][0]['main']['aqi'], 1, 2, 3)
+		air_pm25 = airpollution_policy(items['list'][0]['components']['pm2_5'], 15, 35, 75)
+		air_pm10 = airpollution_policy(items['list'][0]['components']['pm10'], 30, 80, 150)
+		# air_o3 = airpollution_policy(items['list'][0]['components']['o3'], 0.031, 0.061, 0.201)
+		# air_so2 = airpollution_policy(items['list'][0]['components']['so2'], 0.031, 0.061, 0.201)
+
+		self.__dict_data['대기질지수'] = air_total
+		self.__dict_data['미세먼지'] = air_pm10
+		self.__dict_data['초미세먼지'] = air_pm25
+		self.__dict_data['co'] = items['list'][0]['components']['co']
+		self.__dict_data['no'] = items['list'][0]['components']['no']
+		self.__dict_data['no2'] = items['list'][0]['components']['no2']
+		self.__dict_data['o3'] = items['list'][0]['components']['o3']
+		self.__dict_data['so2'] = items['list'][0]['components']['so2']
+		self.__dict_data['pm2_5'] = items['list'][0]['components']['pm2_5']
+		self.__dict_data['pm10'] = items['list'][0]['components']['pm10']
+		self.__dict_data['nh3'] = items['list'][0]['components']['nh3']
 
 
 if __name__ == "__main__":
-	print("main")
-	getNowCity("Incheon")
+	ow = OpenWeatherAPI("c7446d3b017961049805343e08347b43")
+	ow.update_now_auto("Seoul")
+	ow_dict = ow.getter_dict()
 
-	for key in dict_data:
-		print(key + " : %r" % dict_data[key])
-
+	for key in ow_dict:
+		print(key + " : %r" % ow_dict[key])
--- realtimedb_connect.py
+++ realtimedb_connect.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #       [ RealTime Database connect Program ]
-#   수정일 : 2021-08-14
+#   수정일 : 2021-08-15
 #   작성자 : 최현식(chgy2131@naver.com)
 #   변경점 (해야할거)
 #        - 최초작성
@@ -11,12 +11,16 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from module.weather_api import openweathermap_api as ow
+from module.weather_api import openweathermap_api
 from module.slack import slack
 import time
 import json
 
+slack_bot_token = "xoxb-2362622259573-2358980968998-mSTtdyrrEoh7fNjXdYb7wYOX"
 json_file_path = "./realtimedb.json"
+firebase_key_path = './firebase-python-sdk-key/threeamericano-firebase-adminsdk-ejh8q-d74c5b0c68.json'
+firebase_realtimedb_url = 'https://threeamericano-default-rtdb.firebaseio.com/'
+openweathermap_service_key = "c7446d3b017961049805343e08347b43"
 
 
 def alert_error(message):
@@ -30,16 +34,16 @@ def write_jsonfile(file_path, dict_data):
 
 
 # Slack Bot 개체 생성하기
-sl = slack.SlackBot(token="xoxb-2362622259573-2358980968998-mSTtdyrrEoh7fNjXdYb7wYOX")
+sl = slack.SlackBot(token=slack_bot_token)
 
 # 파이어베이스 RealTime DB 연결
-firebase_key_path = './firebase-python-sdk-key/threeamericano-firebase-adminsdk-ejh8q-d74c5b0c68.json'
-
 cred = credentials.Certificate(firebase_key_path)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://threeamericano-default-rtdb.firebaseio.com/'
+    'databaseURL': firebase_realtimedb_url
 })
 
+# OpenWeatherAPI 개체 생성하기
+ow = openweathermap_api.OpenWeatherAPI(service_key=openweathermap_service_key)
 
 ##############################################################################
 #
@@ -48,8 +52,8 @@ firebase_admin.initialize_app(cred, {
 ##############################################################################
 
 # 날씨API를 통해 값 가져오기
-ow.getNowCity("Incheon")
-ow_dict = ow.getterDict()
+ow.update_now_auto("Seoul")
+ow_dict = ow.getter_dict()
 print(ow_dict)
 
 print("===================================")
